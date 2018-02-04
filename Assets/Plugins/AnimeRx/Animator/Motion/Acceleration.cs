e61a358d075e9d5b7f5ffec691ed4898c7b63ee2
kyubuns <kyubuns@gmail.com>
2018-02-04 23:22:07 +0900

    chore: Acceleration

---
+++ Assets/Plugins/AnimeRx/Animator/Motion/Acceleration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AnimeRx
+{
+    public static partial class Motion
+    {
+        public static IAnimator InAcceleration(float velocityMax, float acceleration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IAnimator OutAcceleration(float velocityMax, float acceleration)
+        {
+            throw new NotImplementedException();
+        }
+
+        public static IAnimator InOutAcceleration(float velocityMax, float acceleration)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Assets/Plugins/AnimeRx/Animator/Motion/Uniform.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace AnimeRx
+{
+    public static partial class Motion
+    {
+        public static IAnimator Uniform(float velocity)
+        {
+            return new UniformAnimator(velocity);
+        }
+
+        private class UniformAnimator : IAnimator
+        {
+            private readonly float velocity;
+
+            public UniformAnimator(float velocity)
+            {
+                this.velocity = velocity;
+            }
+
+            public float CalcFinishTime(float distance)
+            {
+                return distance / velocity;
+            }
+
+            public float CalcPosition(float time, float distance)
+            {
+                return Mathf.Clamp01(time * velocity / distance);
+            }
+        }
+    }
+}
