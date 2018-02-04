a4b529f305729dbe90b4b8a027bce721e60243ed
kyubuns <kyubuns@gmail.com>
2018-02-05 00:52:30 +0900

    refactor: ファイル分割

---
+++ Assets/Plugins/AnimeRx/Extension/GameObjectPositionExtensions.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace AnimeRx
+{
+    public static class GameObjectPositionExtensions
+    {
+        public static IDisposable SubscribeToPosition(this IObservable<Vector3> source, GameObject gameObject)
+        {
+            return source.SubscribeToPosition(gameObject.transform);
+        }
+
+        public static IDisposable SubscribeToPosition(this IObservable<Vector2> source, GameObject gameObject)
+        {
+            return source.SubscribeToPosition(gameObject.transform);
+        }
+
+        public static IDisposable SubscribeToPosition(this IObservable<IList<float>> source, GameObject gameObject)
+        {
+            return source.SubscribeToPosition(gameObject.transform);
+        }
+
+        public static IDisposable SubscribeToPositionX(this IObservable<float> source, GameObject gameObject)
+        {
+            return source.SubscribeToPositionX(gameObject.transform);
+        }
+
+        public static IDisposable SubscribeToPositionY(this IObservable<float> source, GameObject gameObject)
+        {
+            return source.SubscribeToPositionY(gameObject.transform);
+        }
+
+        public static IDisposable SubscribeToPositionZ(this IObservable<float> source, GameObject gameObject)
+        {
+            return source.SubscribeToPositionZ(gameObject.transform);
+        }
+
+        public static IObservable<Vector3> DoToPosition(this IObservable<Vector3> source, GameObject gameObject)
+        {
+            return source.DoToPosition(gameObject.transform);
+        }
+
+        public static IObservable<Vector2> DoToPosition(this IObservable<Vector2> source, GameObject gameObject)
+        {
+            return source.DoToPosition(gameObject.transform);
+        }
+
+        public static IObservable<IList<float>> DoToPosition(this IObservable<IList<float>> source, GameObject gameObject)
+        {
+            return source.DoToPosition(gameObject.transform);
+        }
+
+        public static IObservable<float> DoToPositionX(this IObservable<float> source, GameObject gameObject)
+        {
+            return source.DoToPositionX(gameObject.transform);
+        }
+
+        public static IObservable<float> DoToPositionY(this IObservable<float> source, GameObject gameObject)
+        {
+            return source.DoToPositionY(gameObject.transform);
+        }
+
+        public static IObservable<float> DoToPositionZ(this IObservable<float> source, GameObject gameObject)
+        {
+            return source.DoToPositionZ(gameObject.transform);
+        }
+    }
+}
---
+++ Assets/Plugins/AnimeRx/Extension/TransformPositionExtensions.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using UniRx;
+using UnityEngine;
+
+namespace AnimeRx
+{
+    public static class TransformPositionExtensions
+    {
+        public static IDisposable SubscribeToPosition(this IObservable<Vector3> source, Transform transform)
+        {
+            return source.SubscribeWithState(transform, (x, t) => t.position = x);
+        }
+
+        public static IDisposable SubscribeToPosition(this IObservable<Vector2> source, Transform transform)
+        {
+            return source.SubscribeWithState(transform, (x, t) => t.position = x);
+        }
+
+        public static IDisposable SubscribeToPosition(this IObservable<IList<float>> source, Transform transform)
+        {
+            return source.SubscribeWithState(transform, (x, t) => t.position = x.Count >= 3 ? new Vector3(x[0], x[1], x[2]) : new Vector3(x[0], x[1]));
+        }
+
+        public static IDisposable SubscribeToPositionX(this IObservable<float> source, Transform transform)
+        {
+            return source.SubscribeWithState(transform, (x, t) =>
+            {
+                var p = t.position;
+                p.x = x;
+                t.position = p;
+            });
+        }
+
+        public static IDisposable SubscribeToPositionY(this IObservable<float> source, Transform transform)
+        {
+            return source.SubscribeWithState(transform, (x, t) =>
+            {
+                var p = t.position;
+                p.y = x;
+                t.position = p;
+            });
+        }
+
+        public static IDisposable SubscribeToPositionZ(this IObservable<float> source, Transform transform)
+        {
+            return source.SubscribeWithState(transform, (x, t) =>
+            {
+                var p = t.position;
+                p.z = x;
+                t.position = p;
+            });
+        }
+
+        public static IObservable<Vector3> DoToPosition(this IObservable<Vector3> source, Transform transform)
+        {
+            return source.Do(x => transform.position = x);
+        }
+
+        public static IObservable<Vector2> DoToPosition(this IObservable<Vector2> source, Transform transform)
+        {
+            return source.Do(x => transform.position = x);
+        }
+
+        public static IObservable<IList<float>> DoToPosition(this IObservable<IList<float>> source, Transform transform)
+        {
+            return source.Do(x => transform.position = x.Count >= 3 ? new Vector3(x[0], x[1], x[2]) : new Vector3(x[0], x[1]));
+        }
+
+        public static IObservable<float> DoToPositionX(this IObservable<float> source, Transform transform)
+        {
+            return source.Do(x =>
+            {
+                var p = transform.position;
+                p.x = x;
+                transform.position = p;
+            });
+        }
+
+        public static IObservable<float> DoToPositionY(this IObservable<float> source, Transform transform)
+        {
+            return source.Do(x =>
+            {
+                var p = transform.position;
+                p.y = x;
+                transform.position = p;
+            });
+        }
+
+        public static IObservable<float> DoToPositionZ(this IObservable<float> source, Transform transform)
+        {
+            return source.Do(x =>
+            {
+                var p = transform.position;
+                p.z = x;
+                transform.position = p;
+            });
+        }
+    }
+}
