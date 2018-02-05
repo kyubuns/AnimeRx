52d25bbbe926df51b1dd8a526b023579eefcc859
kyubuns <kyubuns@gmail.com>
2018-02-05 14:04:18 +0900

    feat: PackageExporter

---
+++ Assets/Editor/PackageExporter.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace Development
+{
+    public static class PackageExporter
+    {
+        [MenuItem("Dev/Export Package")]
+        public static void Export()
+        {
+            var directories = new[]
+            {
+                "Assets/Plugins/AnimeRx",
+            };
+            var outputPath = Path.Combine(Path.GetDirectoryName(Application.dataPath), "AnimeRx.unitypackage");
+
+            AssetDatabase.ExportPackage(directories, outputPath);
+
+            Debug.LogFormat("ExportPackage {0}", outputPath);
+        }
+    }
+}
