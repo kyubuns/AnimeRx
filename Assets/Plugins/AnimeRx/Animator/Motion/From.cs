96269900d42bdfb5f4964c622b90e2a173ee8e34
kyubuns <kyubuns@gmail.com>
2018-02-05 10:45:36 +0900

    feat: AnimationCurve

---
+++ Assets/Plugins/AnimeRx/Animator/Motion/From.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using UnityEngine;
+
+namespace AnimeRx
+{
+    public static partial class Motion
+    {
+        public static IAnimator From(AnimationCurve curve, TimeSpan duration)
+        {
+            return new AnimationCurveAnimator(curve, duration);
+        }
+
+        private class AnimationCurveAnimator : IAnimator
+        {
+            private readonly AnimationCurve curve;
+            private readonly float duration;
+
+            public AnimationCurveAnimator(AnimationCurve curve, TimeSpan duration)
+            {
+                this.curve = curve;
+                this.duration = (float) duration.TotalSeconds;
+            }
+
+            public float CalcFinishTime(float distance)
+            {
+                return duration;
+            }
+
+            public float CalcPosition(float time, float distance)
+            {
+                return curve.Evaluate(time / duration);
+            }
+        }
+    }
+}
